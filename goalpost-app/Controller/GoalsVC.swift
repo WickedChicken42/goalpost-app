222c61c901cea19adbf729e778769d2e8bbfd517
James Ullom <jamesullom@Jamess-MacBook-Pro-2.local>
2018-09-20 10:11:23 -0600

    Added the ability to delete a goal from storage and to implement editing of the tableview (swiping).

--- goalpost-app/Controller/GoalsVC.swift
+++ goalpost-app/Controller/GoalsVC.swift
@@ -35,16 +35,7 @@ class GoalsVC: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        self.fetch { (complete) in
-            if complete {
-                // If we have any data returned, show/hide the tableview
-                if goals.count > 0 {
-                    tableView.isHidden = false
-                } else {
-                    tableView.isHidden = true
-                }
-            }
-        }
+        fetchCoreDataObjects()
         
         tableView.reloadData()
     }
@@ -58,7 +49,21 @@ class GoalsVC: UIViewController {
         presentDetail(createGoalVC)
         
     }
-    
+
+    func fetchCoreDataObjects() {
+        
+        self.fetch { (complete) in
+            if complete {
+                // If we have any data returned, show/hide the tableview
+                if goals.count > 0 {
+                    tableView.isHidden = false
+                } else {
+                    tableView.isHidden = true
+                }
+            }
+        }
+
+    }
 }
 
 // Creating an extention of this VC to support the tableview implementation requirements - I LIKE THIS MUCH!!!
@@ -85,12 +90,44 @@ extension GoalsVC: UITableViewDelegate, UITableViewDataSource {
         return cell
 
     }
+ 
+    // Added to support the swiping of row items to access their Actions - Allows editing
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+
+    // Added to support the swiping of row items to access their Actions - Won't show any special icons
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return .none
+    }
     
+    // Added to support the swiping of row items to access their Actions - Defining the actions
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+
+        // Define the Delete action shown when swiping on a row item
+        let deleteAction = UITableViewRowAction(style: .destructive, title: "DELETE") { (rowAction, indexPath) in
+            // What we will do when the Delete action is pressed (or full swiped)
+            // Removes the goal from persistent storage
+            self.removeGoal(atIndexPath: indexPath)
+            // Reload the local goals array from persisten storage
+            self.fetchCoreDataObjects()
+            // Remove the deleted goal from the table view
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+
+        deleteAction.backgroundColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
+
+        var actions: [UITableViewRowAction] = []
+        actions.append(deleteAction)
+
+        return actions
+    }
 }
 
 // Creating this extension to define the fetch function that will pull our goals from persistent storage
 extension GoalsVC {
     
+    // Retreives the data from persisten storage and loads them to the local goals array
     func fetch(completion: (_ complete: Bool) -> ()) {
         
         // Get the managed context for this app
@@ -111,4 +148,23 @@ extension GoalsVC {
         }
         
     }
+    
+    // Used to remove a specific goal from our data based on the indexPath being passed in
+    func removeGoal(atIndexPath: IndexPath) {
+        
+        // Get the managed context for this app
+        guard let managedContext = appDelegate?.persistentContainer.viewContext else { return }
+
+        // Delete the item from our managed context
+        managedContext.delete(goals[atIndexPath.row])
+        
+        // Perform a Do/Try/Catch to save the context which will remove the deleted item from storage
+        do {
+            try managedContext.save()
+            print("Successfully deleted a goal!")
+        } catch {
+            debugPrint("Could not delete a goal: \(error.localizedDescription)")
+        }
+    }
+
 }
